c8ef04d17f323b0f3fddaaebe76f2076280d8a2e
Ingarbenonisen <70366351+Ingarbenonisen@users.noreply.github.com>
2020-09-22 12:06:10 +0200

    oppg2 cmt

--- src/Oblig1Oppgave2.java
+++ src/Oblig1Oppgave2.java
@@ -1,2 +1,17 @@
 public class Oblig1Oppgave2 {
+
+
+        public static void main(String[] args){
+            // les inn navn, antall og pris for vare 1
+
+            // les inn navn, antall og pris for vare 2
+
+            // bruk avvikshåntering (try/catch) for å konvertere
+            // antall og pris for vare 1 og vare 2
+
+            // opprett to vare-objekter med de innleste verdiene
+
+            // beregn og skriv ut total pris for alle varer i System.out
+        }
+
 }
