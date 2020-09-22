ef57f6246f6ab6913d34635a86bcb0b2594f7f7e
Ingarbenonisen <70366351+Ingarbenonisen@users.noreply.github.com>
2020-09-22 13:04:21 +0200

    oppg2 cmt

--- src/Oblig1Oppgave2.java
+++ src/Oblig1Oppgave2.java
@@ -1,11 +1,42 @@
+import javax.swing.*;
+
 public class Oblig1Oppgave2 {
 
 
         public static void main(String[] args){
                 
-            // les inn navn, antall og pris for vare 1
+                String innNavn= JOptionPane.showInputDialog("Skriv inn et navn");
+                String innAntall= JOptionPane.showInputDialog("Skriv inn antall varer");
+                int antall;
+                try{
+                        Integer.parseInt(innAntall);
+                }
+                catch (Exception e){
+                        antall = 0;
+                }
+
+                String innPris= JOptionPane.showInputDialog("SKriv inn en pris");
+                int pris;
+                try{
+                        Integer.parseInt(innPris);
+                }
+                catch (Exception e){
+                        pris = 0;
+                }
+
+                // les inn navn, antall og pris for vare 1
+
+                Vare vare1 = new Vare();
+                vare1.navn= innNavn;
+                vare1.antall= innAntall;
+                vare1.pris = pris;
 
             // les inn navn, antall og pris for vare 2
+
+                Vare vare2 = new Vare();
+                vare2.navn= innNavn;
+                vare2.antall= innAntall;
+                vare2.pris = pris;
 
             // bruk avvikshåntering (try/catch) for å konvertere
             // antall og pris for vare 1 og vare 2
@@ -13,6 +44,8 @@ public class Oblig1Oppgave2 {
             // opprett to vare-objekter med de innleste verdiene
 
             // beregn og skriv ut total pris for alle varer i System.out
+
+
         }
 
 }
