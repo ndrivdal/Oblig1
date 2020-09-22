ee5c99d0a60467f541459bf6a233868ae81cf2de
Ingarbenonisen <70366351+Ingarbenonisen@users.noreply.github.com>
2020-09-22 12:24:52 +0200

    oppg2 cmt

--- src/Oblig1Oppgave1.java
+++ src/Oblig1Oppgave1.java
@@ -1,7 +1,10 @@
+import javax.swing.*;
+import java.util.jar.JarOutputStream;
+
 public class Oblig1Oppgave1 {
     public static void main(String[] args) {
         //  Sett en variabel lik en verdi for radius
-
+    double radius = Double.parseDouble(JOptionPane.showInputDialog("SKriv inn radius: "));
 
     /* Kall så de statiske metodene i sirkel-klassen for å vise følgende på
        System.out (gitt radius lik 23.5)
@@ -12,6 +15,15 @@ public class Oblig1Oppgave1 {
 
        Tips : String.format("%.2f",desimalverdi); for å formatere to desimaler
     */
+        double diameter = Sirkel.diameter(radius);
+        System.out.println(" Diameteren til sirkelen er med radius "+radius+" er "+diameter);
+
+        double areal = Sirkel.areal(radius);
+        System.out.println("Arealet av sirkelen med radius "+radius+" er "+areal);
+
+        double omkrets = Sirkel.omkrets(radius);
+        System.out.println("Omkretsen av sirkelen med radius "+radius+" er "+omkrets);
+
 
     }
 }
