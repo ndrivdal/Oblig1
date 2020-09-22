9f8a67c68ab4e7f8f9c514b252ebeb3d659893df
ndrivdal <ndrivdal@gmail.com>
2020-09-22 14:15:12 +0200

    Commit

--- src/Oblig1Oppgave2.java
+++ src/Oblig1Oppgave2.java
@@ -1,49 +1,91 @@
-import javax.swing.*;
+import static javax.swing.JOptionPane.*;
 
 public class Oblig1Oppgave2 {
-        public static void main(String[] args){
-                
-                String innNavn= JOptionPane.showInputDialog("Skriv inn et navn");
-                String innAntall= JOptionPane.showInputDialog("Skriv inn antall varer");
-                int antall;
-                try{
-                        Integer.parseInt(innAntall);
-                }
-                catch (Exception e){
-                        antall = 0;
-                }
+    public static void main(String[] args){
 
-                String innPris= JOptionPane.showInputDialog("Skriv inn en pris");
-                int pris;
-                try{
-                        Integer.parseInt(innPris);
-                }
-                catch (Exception e){
-                        pris = 0;
-                }
-
-                // les inn navn, antall og pris for vare 1
-
-                Vare vare1 = new Vare();
-                vare1.navn= innNavn;
-                vare1.antall= antall;
-                vare1.pris = pris;
-
-            // les inn navn, antall og pris for vare 2
-
-                Vare vare2 = new Vare();
-                vare2.navn= innNavn;
-                vare2.antall= antall;
-                vare2.pris = pris;
-
-            // bruk avvikshåntering (try/catch) for å konvertere
-            // antall og pris for vare 1 og vare 2
-
-            // opprett to vare-objekter med de innleste verdiene
-
-            // beregn og skriv ut total pris for alle varer i System.out
-
-
+        //String innNavn = showInputDialog("Skriv inn navn på vare");
+        //String innAntall = showInputDialog("Skriv inn antall varer");
+        int antall = 0;
+        try{
+            Integer.parseInt(innAntall);
         }
+        catch (Exception e){
+            antall = 0;
+        }
+
+        String innPris = showInputDialog("Skriv inn en pris");
+        int pris = 0;
+        try{
+            Integer.parseInt(innPris);
+        }
+        catch (Exception e){
+            pris = 0;
+        }
+
+
+        Vare vare1 = new Vare();
+        vare1.navn= innNavn;
+        vare1.antall= antall;
+        vare1.pris = pris;
+
+        Vare vare2 = new Vare();
+        vare2.navn= innNavn;
+        vare2.antall= antall;
+        vare2.pris = pris;
+
+
+
+
+        // les inn navn, antall og pris for vare 1
+        String innNavn1 = showInputDialog("Skriv inn navn på vare 1");
+        String innAntall1 = showInputDialog("Skriv inn antall varer");
+        String innPris1 = showInputDialog("Skriv pris på vare 1");
+
+        // les inn navn, antall og pris for vare 2
+        String innNavn2 = showInputDialog("Skriv inn navn på vare 2");
+        String innAntall2 = showInputDialog("Skriv inn antall varer");
+        String innPris2 = showInputDialog("Skriv inn pris på vare 2");
+
+        // bruk avvikshåntering (try/catch) for å konvertere
+        // antall og pris for vare 1 og vare 2
+        // String navn1, navn2;
+        int antall1, antall2;
+        double pris1, pris2;
+
+        try {
+            antall1 = Integer.parseInt(innAntall1);
+        }
+        catch(Exception e) {
+            antall1 = 1;
+        }
+
+        try {
+            antall2 = Integer.parseInt(innAntall2);
+        }
+        catch(Exception e) {
+            antall2 = 1;
+        }
+
+        try {
+            pris1 = Double.parseDouble(innPris1);
+        }
+        catch(Exception e) {
+            pris1 = 0;
+        }
+
+        try {
+            pris2 = Double.parseDouble(innPris2);
+        }
+        catch(Exception e) {
+            pris2 = 0;
+        }
+
+        // opprett to vare-objekter med de innleste verdiene
+
+        // beregn og skriv ut total pris for alle varer i System.out
+        double totalPris = vare1.totalPris() + vare2.totalPris();
+        System.out.println(totalPris);
+
+    }
 
 }
--- src/Vare.java
+++ src/Vare.java
@@ -1,15 +1,18 @@
 public class Vare {
-    private String navn;
-    private int antall;
-    private double pris;
+    public String navn;
+    public int antall;
+    public double pris;
 
     // Konstruktør med alle attributter
 
 
     // get / set metoder
+
     // antall og pris må være større enn null
 
     // metode for å beregne totalpris (pris * antall)
-
-    // Halla
+    public double totalPris() {
+        double utPris = pris * antall;
+        return utPris;
+    }
 }
