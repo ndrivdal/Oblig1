1d3563fb2d919a1d4c4b1a71923a784a28e509fa
ndrivdal <ndrivdal@gmail.com>
2020-09-23 12:13:16 +0200

    .

--- src/Oblig1Oppgave2.java
+++ src/Oblig1Oppgave2.java
@@ -52,7 +52,7 @@ public class Oblig1Oppgave2 {
 
         // beregn og skriv ut total pris for alle varer i System.out
         double totalPris = vare1.totalPris() + vare2.totalPris();
-        System.out.println(totalPris);
+        System.out.println("Totalprisen ble: " + totalPris + " kr");
     }
 
 }
